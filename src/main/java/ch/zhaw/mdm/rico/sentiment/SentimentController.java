e5ead35da5a3121f41322e05998a04c83326093d
Rico Staedeli ZHAW <staedric@students.zhaw.ch>
2023-04-15 12:35:52 +0200

    first commit

---
+++ src/main/java/ch/zhaw/mdm/rico/sentiment/SentimentController.java
@@ -0,0 +1,16 @@
+package ch.zhaw.mdm.rico.sentiment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SentimentController {
+
+    private SentimentAnalysis analysis = new SentimentAnalysis();
+
+    @GetMapping("/sentiment")
+    public String predict(@RequestParam(name="text", required = true) String text) throws Exception {
+        var result = analysis.predict(text);
+        return result.toJson();
+    }
+}
